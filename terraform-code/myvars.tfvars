3c2bab2609cec7048a554765a0c9b7f712381ff4
venusmailsgit <152349576+venusmailsgit@users.noreply.github.com>
2024-06-25 17:08:55 +0530

    Add files via upload

---
+++ terraform-code/myvars.tfvars
@@ -0,0 +1,15 @@
+/*vpc values*/
+vpc_cidr = "10.10.0.0/16"
+vpc_name = "myvpc"
+igw_name = "myvpc-igw"
+route_name = "myvpc-route1"
+vpc_sub_cidr = "10.10.15.0/24"
+sub_name = "myvpc-sub1"
+sec_grp_name = "myvpc-sg"
+
+/*ec2 values*/
+ami_id="ami-0eaf7c3456e7b5b68"
+ec2_size="t2.micro"
+ec2_name="myserver1"
+ec2_key="tfkey1"
+
---
+++ terraform-code/variables.tf
@@ -0,0 +1,15 @@
+/*vpc variables*/
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "igw_name" {}
+variable "route_name" {}
+variable "vpc_sub_cidr" {}
+variable "sub_name" {}
+variable "sec_grp_name" {}
+
+/*ec2 variables*/
+variable "ami_id" {}
+variable "ec2_size" {}
+variable "ec2_name" {}
+variable "ec2_key" {}
+
